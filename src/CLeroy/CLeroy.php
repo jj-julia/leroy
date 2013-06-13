e30ba7563be57324e8f9b5d7947a3159bb8d53bd
JULIA WELLS <juwe12@sweet.student.bth.se>
2013-06-13 11:09:31 +0200

    Correcting errors in CLeroy regarding new object class

--- src/CLeroy/CLeroy.php
+++ src/CLeroy/CLeroy.php
@@ -33,35 +33,39 @@ class CLeroy implements ISingleton {
   /**
    * Frontcontroller, check url and route to controllers.
    */
-  public function FrontControllerRoute() {
+ public function FrontControllerRoute() {
     // Take current url and divide it in controller, method and parameters
-    $this->request = new CRequest();
+    $this->request = new CRequest($this->config['url_type']);
     $this->request->Init($this->config['base_url']);
     $controller = $this->request->controller;
     $method     = $this->request->method;
     $arguments  = $this->request->arguments;
     
     // Is the controller enabled in config.php?
-    $controllerExists    = isset($this->config['controllers'][$controller]);
-    $controllerEnabled   = false;
-    $className           = false;
-    $classExists         = false;
+    $controllerExists   = isset($this->config['controllers'][$controller]);
+    $controllerEnabled  = false;
+    $className          = false;
+    $classExists        = false;
 
     if($controllerExists) {
-      $controllerEnabled   = ($this->config['controllers'][$controller]['enabled'] == true);
-      $className           = $this->config['controllers'][$controller]['class'];
-      $classExists         = class_exists($className);
+      $controllerEnabled  = ($this->config['controllers'][$controller]['enabled'] == true);
+      $className          = $this->config['controllers'][$controller]['class'];
+      $classExists        = class_exists($className);
     }
     
     // Check if controller has a callable method in the controller class, if then call it
     if($controllerExists && $controllerEnabled && $classExists) {
-      $formattedMethod = str_replace(array('_', '-'), '', $method);
       $rc = new ReflectionClass($className);
       if($rc->implementsInterface('IController')) {
-        if($rc->hasMethod($method)) {
+         $formattedMethod = str_replace(array('_', '-'), '', $method);
+        if($rc->hasMethod($formattedMethod)) {
           $controllerObj = $rc->newInstance();
-          $methodObj = $rc->getMethod($method);
-          $methodObj->invokeArgs($controllerObj, $arguments);
+          $methodObj = $rc->getMethod($formattedMethod);
+          if($methodObj->isPublic()) {
+            $methodObj->invokeArgs($controllerObj, $arguments);
+          } else {
+            die("404. " . get_class() . ' error: Controller method not public.');          
+          }
         } else {
           die("404. " . get_class() . ' error: Controller does not contain method.');
         }
@@ -73,7 +77,6 @@ class CLeroy implements ISingleton {
       die('404. Page is not found.');
     }
   }
-  
   
   /**
    * ThemeEngineRender, renders the reply of the request to HTML or whatever.
--- src/CObject/CObject.php
+++ src/CObject/CObject.php
@@ -1,19 +1,19 @@
 <?php
 /**
-* Holding a instance of CLydia to enable use of $this in subclasses.
-*
-* @package LeroyCore
-*/
+ * Holding a instance of CLeroy to enable use of $this in subclasses.
+ *
+ * @package LeroyCore
+ */
 class CObject {
 
-   public $config;
-   public $request;
-   public $data;
+  public $config;
+  public $request;
+  public $data;
 
-   /**
-    * Constructor
-    */
-   protected function __construct() {
+  /**
+   * Constructor
+   */
+  protected function __construct() {
     $ly = CLeroy::Instance();
     $this->config   = &$ly->config;
     $this->request  = &$ly->request;
